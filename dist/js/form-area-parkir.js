797c4d49186abdcb2db860d6024fba268172bb62
KafiRijal <kafirs75@gmail.com>
2024-01-12 20:09:46 +0700

    update kafi

---
+++ dist/js/form-area-parkir.js
@@ -0,0 +1,15 @@
+function data_parkir() {
+    let area = document.getElementById("nama_area").value;
+    if (area.length === 0 || area.length > 30) {
+        alert('Nama area harus diisi dan maksimum 30 karakter');
+        return false;
+    }
+    let kapasitas = document.getElementById("kapasitas").value;
+    let kapasitasregex = /^\d{10,}$/;
+    if (!kapasitasregex.test(kapasitas)) {
+        alert('Kapasitas harus berupa angka');
+        return false;
+    }
+
+    window.alert("Berhasil Submit");
+}
